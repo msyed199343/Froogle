502370635d5964e9ccbee093ea664489583154ae
Syed Mohiuddin <msyed199343@gmail.com>
2020-01-27 15:06:18 -0600

    added more pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,19 @@ class UsersController < ApplicationController
     def show
         render :about
     end
+
+    def create 
+        #if user exists show them to this page 
+        #render :login
+        #else if they dont exist show them to the sign up page below
+        #if user is logged in buttun should say sign out (send to destroy to destory session)
+    end
+
+    def new
+        render :sign_up
+    end
+
+    def destroy
+        #ends current session
+    end
 end
