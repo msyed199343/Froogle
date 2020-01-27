9a2ecbf27c8f0ae034518d2e615443bf7765c819
Syed Mohiuddin <msyed199343@gmail.com>
2020-01-26 23:05:23 -0600

    set up initial

--- db/migrate/20200126042912_create_users.rb
+++ db/migrate/20200126042912_create_users.rb
@@ -1,11 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :username, Null: false
-      t.string :password_digest, Null: false
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      
+      t.timestamps
 
-      t.index
     end
-    t.timestamps
+    
+  
   end
 end
