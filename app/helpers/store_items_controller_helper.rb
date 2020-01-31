07f228d94e3ed304c2c96275ac9fa982075112d8
Syed Mohiuddin <msyed199343@gmail.com>
2020-01-31 17:14:37 -0600

    edits

--- app/helpers/store_items_controller_helper.rb
+++
@@ -1,2 +0,0 @@
-module StoreItemsControllerHelper
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  
+   resources :users
 
   get '/store_items/about', to: 'store_items#about'
   get '/store_items/store_front', to: 'store_items#store_front'
   resources :store_items 
   resource :session, only: [:new, :create, :destroy]
-  resources :users
+ 
   
 root 'welcome#index'
 end
