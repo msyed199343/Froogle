6c540d03d0604f81167b7d9d2ceae32e225cfc2e
Syed Mohiuddin <msyed199343@gmail.com>
2020-02-15 03:22:58 -0600

    Made search terms more dynamic

--- app/controllers/store_items_controller.rb
+++ app/controllers/store_items_controller.rb
@@ -4,25 +4,45 @@ class StoreItemsController < ApplicationController
     helper_method :store_front, :about, :random
     
     def index
-        # @items = StoreItem.all 
+        #@items = StoreItem.all 
         render :home2
     end
 
     def show
-       
-      
-            @store_item = StoreItem.find_by(name: params[:name])
 
+            corrected_word = spell_check(params[:name]) 
+            @store_item = StoreItem.find_by(name: corrected_word)
+
+            
 
                 
             if @store_item
-                    @response = response
-                        render :"#{@store_item[:name]}"  
 
-             else
-                            
+                render :"#{@store_item[:name]}"  
+                  
+            else
+                    
                 redirect_to "/store_items/store_front/"
+                
             end
+    end
+
+    def spell_check(word)
+        
+            split = word.split("")
+            split.delete(" ")
+            count = 0
+            bundled_letters = ""
+            
+            split.each do |letter|
+                bundled_letters += letter 
+                if bundled_letters.length == 3 || word.length < 3
+                    bl = bundled_letters.capitalize
+                    @item = StoreItem.select("name").where(" name LIKE ?", "%" + "#{bl}" + "%") 
+                end
+            end
+            
+        @item 
     end
 
     def random 
